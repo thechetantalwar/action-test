8ca9cc4b5ac93269123e1566cad3b7a06291c0a0
Chetan <chetan.talwaar@gmail.com>
2021-09-24 09:21:45 +0530

    Action Updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-text="{'text':""'${2}'""}"
-echo $text
-curl -H 'Content-Type: application/json' -d "'${text}'" $1
+text='{"text":"'${2}'"}'
+echo $text > test.txt
 #echo curl -X POST -H 'Content-type: application/json' --data $text $1
+curl $1 -X POST -H 'Content-type: application/json' --data "@test.txt" 

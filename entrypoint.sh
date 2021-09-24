c85eea0ed89057639fdbe054773730bfea75a9fe
Chetan <chetan.talwaar@gmail.com>
2021-09-24 20:45:21 +0530

    Action Updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-text='{"text":"Build Number '${2}'"}'
+text='{"text":"'${2}'"}'
 echo $text > test.txt
 #echo curl -X POST -H 'Content-type: application/json' --data $text $1
 curl $1 -X POST -H 'Content-type: application/json' --data "@test.txt" 
